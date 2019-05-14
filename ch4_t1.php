470e48168c76d1af062eb026d1913733539ba6d1
Myat Hsu <myat.hsu.mon@digitallaboratory.net>
2019-05-14 10:15:46 +0630

    card-21469502 - Chapter4

--- ch4_t1.php
+++ ch4_t1.php
@@ -1,72 +1,74 @@
 <?php
-$vegetables = array( 'corn' => 'yellow',
-'beet'=> 'red', 'carrot'=> 'orange');
-
-
-$juice=[0=>'apple',1=>'mango'];
-$num=count($vegetables);
-print "count ==> $num";
-
-
-$meal = array('breakfast' => 'Walnut Bun', 'lunch' => "chashwe Nut", 'snack'=>'Mulberries', 'dinner' => 'Eggplant');
+$vegetables = array( 
+	'corn' => 'yellow', 
+	'beet'=> 'red', 
+	'carrot' => 'orange'
+);
+$juice = [0 => 'apple', 1 => 'mango'];
+$num = count($vegetables);
+print "count no: $num";
+$meal = array( 
+	'breakfast' => 'Walnut Bun', 
+	'lunch' => "chashwe Nut", 
+	'snack'=>'Mulberries', 
+	'dinner' => 'Eggplant'
+);
 print "<table>\n";
-foreach ($meal as $key=>$value){
-	print "<tr><td>$key</td><td>$value</td></tr>\n";
+foreach ( $meal as $key=>$value ) {
+	print "<tr><td> $key </td><td> $value </td></tr>\n";
 }
 print '</table>';
 ?>
-
 <br><br>
+
 <?php
-$rowstyle=array('even','odd');
-$index=0;
-$meal = array('breakfast' => 'Walnut Bun', 'lunch' => "chashwe Nut", 'snack'=>'Mulberries', 'dinner' => 'Eggplant');
+$rowstyle = array( 'even', 'odd' );
+$index = 0;
+$meal = array(
+	'breakfast' => 'Walnut Bun', 
+	'lunch' => 'chashwe Nut', 
+	'snack'=> 'Mulberries', 
+	'dinner' => 'Eggplant');
 print "<table>\n";
-foreach ($meal as $key=>$value){
-	print '<tr class="'.$rowstyle[$index].'">';
-	print "<td>$key</td><td>$value</td></tr>\n";
-	$index=1-$index;
+foreach ( $meal as $key => $value ) {
+	print '<tr class = "' . $rowstyle[$index] . '">';
+	print "<td> $key </td><td> $value </td></tr>\n";
+	$index = 1 - $index;
 }
 print '</table>';
 ?>
-
 <br><br>
 
 <?php
-
-$d=['breakfast','lunch','dinner'];
-$total=count($d);
-echo "total is $total";
+$d = ['breakfast', 'lunch', 'dinner'];
+$total = count( $d );
+echo 'total is $total';
 ?>
-
-
 <?php
-$rowstyle=array('odd','even');
+$rowstyle = array('odd', 'even');
 $index=0;
-$meal=['breakfast'=>'bread','lunch'=>'rice','dinner'=>'salad'];
+$meal=['breakfast' => 'bread', 'lunch' =>' rice', 'dinner' => 'salad'];
 print "<table>";
-	foreach($meal as $key=>$value){
-		print '<tr class="'.$rowstyle[$index].'">';
-		print "<td>$key</td><td>$value</td></tr>\n";
-	$index=1-$index;
+	foreach( $meal as $key => $value ) {
+		print '<tr class = "' . $rowstyle[$index] . '">';
+		print "<td> $key </td><td> $value </td></tr>\n";
+	$index = 1 - $index;
 	}
 print "</table>";
 ?>
-
 <br><br>
 
 <?php
-$meals=array('walnut bun' =>1, 
-			'cashew nuts and white mushrooms' => 4.95,
-			'Dried mulberries'=>3.00, 
-			'Eggplant with chili sauce'=>6.85);
-foreach ($meals as $dish => $price) { 
-// $price = $price * 2 does NOT work
+$meals = array(
+	'walnut bun' => 1, 
+	'cashew nuts and white mushrooms' => 4.95,
+	'Dried mulberries' => 3.00, 
+	'Eggplant with chili sauce' => 6.85
+);
+foreach ( $meals as $dish => $price ) { 
  $meals[$dish] = $meals[$dish] * 2;
 }
-
-foreach ($meals as $dish => $price) {
-//printf("The new price of %s is \$%.2f.\n",$dish,$price);
+foreach ( $meals as $dish => $price) {
 print "$dish ==> $price";
 print "<br>";
 }
--- ch4_t2.php
+++ ch4_t2.php
@@ -1,13 +1,12 @@
 <?php
-$dinner=['Sweet corn and asparagus', 'lemon chicken','Braised bamboo fungus'];
+$dinner=['Sweet corn and asparagus', 'lemon chicken', 'Braised bamboo fungus'];
 
-for($i=0;$i<count($dinner);$i++)
+for ( $i = 0; $i < count( $dinner ); $i++)
 {
-	print "Dish no $i is $dinner[$i]";
+	print "Dish no $i is $dinner[ $i ]";
 	print "<br>";
 }
 ?>
-
 
 <?php
 $dinner=['Sweet corn and asparagus', 'lemon chicken','Braised bamboo fungus'];
@@ -18,43 +17,44 @@ foreach ($dinner as $dish)
 	print "<br>";
 }
 ?>
-
-
 <br><br>
 
 <?php
-
-$meals = array('Walnut Bun' => 1,
-'Cashew Nuts and White Mushrooms' => 4.95,
-'Dried Mulberries' => 3.00,
-'Eggplant with Chili Sauce' => 6.50,
-'Shrimp Puffs' => 0);
- // Shrimp Puffs are free!
+$meals = array(
+	'Walnut Bun' => 1,
+	'Cashew Nuts and White Mushrooms' => 4.95,
+	'Dried Mulberries' => 3.00,
+	'Eggplant with Chili Sauce' => 6.50,
+	'Shrimp Puffs' => 0
+);
 $books = array("The Eater's Guide to Chinese Characters", 'How to Cook and Eat in Chinese');
 
-if(array_key_exists('Shrimp Puffs',$meals)){
+if ( array_key_exists ('Shrimp Puffs', $meals ) ) {
 	print "yes, we have shrimp puffs";
 }
-else print "no, we don't have."
+else {
+	print "no, we don't have."
+}
 ?>
 
-
-
-
 <?php
-$meals = array('Walnut Bun' => 1,
-'Cashew Nuts and White Mushrooms' => 4.95,
-               'Dried Mulberries' => 3.00,
-               'Eggplant with Chili Sauce' => 6.50,
-               'Shrimp Puffs' => 0);
-$books = array("The Eater's Guide to Chinese Characters", 'How to Cook and Eat in Chinese');
-// This is true: key Dried Mulberries has value 3.00
-if (in_array(3, $meals)) { print 'There is a $3 item.';
+$meals = array(
+	'Walnut Bun' => 1,
+	'Cashew Nuts and White Mushrooms' => 4.95,
+    'Dried Mulberries' => 3.00,
+    'Eggplant with Chili Sauce' => 6.50,
+    'Shrimp Puffs' => 0
+);
+$books = array(
+	"The Eater's Guide to Chinese Characters", 
+	"How to Cook and Eat in Chinese");
+if ( in_array( 3, $meals ) ) {
+	print 'There is a $3 item.';
 }
-// This is true
-if (in_array('How to Cook and Eat in Chinese', $books)) { print "We have How to Cook and Eat in Chinese";
+if (in_array( "How to Cook and Eat in Chinese", $books)) { 
+	print "We have How to Cook and Eat in Chinese";
 }
-// This is false: in_array() is case-sensitive
-if (in_array("the eater's guide to chinese characters", $books)) { print "We have the Eater's Guide to Chinese Characters.";
+if (in_array( "the eater's guide to chinese characters", $books ) ) { 
+	print "We have the Eater's Guide to Chinese Characters.";
 }
 ?>
