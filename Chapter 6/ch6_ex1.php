9f27b6223ffaafed15d9f5e421bf20495afa6935
Myat Hsu <myat.hsu.mon@digitallaboratory.net>
2019-05-20 09:02:09 +0630

    #card-21720183 - PHP Chapter 6

---
+++ Chapter 6/ch6_ex1.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * Create a class called Ingredient
+ */
+class Ingredient {
+	protected $name;
+	protected $cost;
+
+	/**
+	 * Constuctor
+	 * @param string $name
+	 * @param int $cost
+	 */
+	public function __construct( $name, $cost ) {
+		$this->name = $name;
+		$this->cost = $cost;
+	}
+
+	public function getName() {
+		return $this->name;
+	}
+
+	public function getCost() {
+		return $this->cost;
+	}
+}
+?>
---
+++ Chapter 6/ch6_t5.php
@@ -0,0 +1,6 @@
+<?php
+include 'ch6_t4.php';
+use Tiny\fruit as snack;
+\Tiny\fruit::munch( "apple" );
+snack::munch("strawberry");
+?>
